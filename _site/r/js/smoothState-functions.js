8bc8d472082228fe76142a0dc503855c64ad02b2
Joe Shoop <shoopj@gmail.com>
2016-03-03 15:41:47 -0800

    Pushing an update to get some feedback

--- _site/r/js/smoothState-functions.js
+++ _site/r/js/smoothState-functions.js
@@ -1,20 +1,13 @@
-;(function($) {
-  'use strict';
-  var $body = $('html, body'),
-      content = $('#main').smoothState({
-        // Runs when a link has been activated
+; (function ($) {
+    'use strict';
+    var $body   = $('html, body'),
+        content = $('#main').smoothState({
         onStart: {
-          duration: 250, // Duration of our animation
-          render: function (url, $container) {
-            // toggleAnimationClass() is a public method
-            // for restarting css animations with a class
-            content.toggleAnimationClass('is-exiting');
-            // Scroll user to the top
-            $body.animate({
-              scrollTop: 0
-            });
-          }
+            duration: 1000,
+            render: function (url, $container) {
+                content.toggleAnimationClass('is-exiting');
+                $body.animate({ 'scrollTop': 0 });
+            }
         }
-      }).data('smoothState');
-      //.data('smoothState') makes public methods available
+    }).data('smoothState');
 })(jQuery);
